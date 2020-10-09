c65e7e2f2cfb1f56526727fbec7fb6713d49581d
Evgenii Stratonikov <evgeniy@nspcc.ru>
2020-10-09 10:44:31 +0300

    oracle: reprocess request on fail

--- pkg/oracle/oracle.go
+++ pkg/oracle/oracle.go
@@ -46,6 +46,8 @@ type (
 		// respMtx protects responses map.
 		respMtx   sync.RWMutex
 		responses map[uint64]*incompleteTx
+		// removed contains ids of requests which won't be processed further due to expiration.
+		removed map[uint64]bool
 
 		wallet *wallet.Wallet
 	}
@@ -81,6 +83,9 @@ const (
 
 	// defaultRequestTimeout is default request timeout.
 	defaultRequestTimeout = time.Second * 5
+
+	// defaultMaxTaskTimeout is default timeout for the failed request to be reprocessed.
+	defaultMaxTaskTimeout = time.Minute * 3
 )
 
 // NewOracle returns new oracle instance.
@@ -91,6 +96,7 @@ func NewOracle(cfg Config) (*Oracle, error) {
 		close:      make(chan struct{}),
 		requestMap: make(chan map[uint64]*state.OracleRequest, 1),
 		responses:  make(map[uint64]*incompleteTx),
+		removed:    make(map[uint64]bool),
 	}
 	o.oracleScript, o.oracleHash = state.CreateNativeContractHash(nativenames.Oracle)
 	if o.MainCfg.RequestTimeout == 0 {
@@ -100,6 +106,9 @@ func NewOracle(cfg Config) (*Oracle, error) {
 		o.MainCfg.MaxConcurrentRequests = defaultMaxConcurrentRequests
 	}
 	o.requestCh = make(chan request, o.MainCfg.MaxConcurrentRequests)
+	if o.MainCfg.MaxTaskTimeout == 0 {
+		o.MainCfg.MaxTaskTimeout = defaultMaxTaskTimeout
+	}
 
 	var err error
 	w := cfg.MainCfg.UnlockWallet
@@ -147,10 +156,36 @@ func (o *Oracle) Run() {
 	for i := 0; i < o.MainCfg.MaxConcurrentRequests; i++ {
 		go o.runRequestWorker()
 	}
+
+	tick := time.NewTicker(o.MainCfg.MaxTaskTimeout / 2)
 	for {
 		select {
 		case <-o.close:
+			tick.Stop()
 			return
+		case <-tick.C:
+			var reprocess []uint64
+			o.respMtx.RLock()
+			o.removed = make(map[uint64]bool)
+			for id, incTx := range o.responses {
+				incTx.RLock()
+				if incTx.isSent || time.Since(incTx.time) > o.MainCfg.MaxTaskTimeout {
+					if incTx.attempts <= 1 { // reprocess first time
+						reprocess = append(reprocess, id)
+					} else {
+						o.removed[id] = true
+					}
+				}
+				incTx.RUnlock()
+			}
+			for id := range o.removed {
+				delete(o.responses, id)
+			}
+			o.respMtx.Unlock()
+
+			for _, id := range reprocess {
+				o.requestCh <- request{ID: id}
+			}
 		case reqs := <-o.requestMap:
 			for id, req := range reqs {
 				o.requestCh <- request{
